7b34988bc2c48882744167f234643bb6add6a9c4
nam-yeun-hwa <nyh2ya@gmail.com>
2024-07-21 17:45:40 +0900

    fix(post):tdd id값 수정

---
+++ _next/static/chunks/app/layout-8d1cf65272249fca.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6382:function(n,e,t){Promise.resolve().then(t.t.bind(t,3385,23)),Promise.resolve().then(t.t.bind(t,9519,23))},3385:function(){},9519:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=6382)}),_N_E=n.O()}]);
